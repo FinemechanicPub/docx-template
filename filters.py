d644c416ef340dfad62db5f421c2a1cd162d195b
Alexander Rubtsov <averoesmsk@gmail.com>
2023-09-22 23:01:23 +0300

    Move filters to a separate file

---
+++ filters.py
@@ -0,0 +1,22 @@
+"""Дополнительные фильтры для Jinja2"""
+
+
+def zs(value):
+    if not value:
+        return ""
+    else:
+        return int(round(value, 0))
+
+
+def d(value):
+    if value is None:
+        return "-"
+    else:
+        return int(round(value, 0))
+
+
+def zd(value):
+    if not value:
+        return "-"
+    else:
+        return int(round(value, 0))
--- make_doc.py
+++ make_doc.py
@@ -1,11 +1,11 @@
+from inspect import getmembers, isfunction
 from sys import argv, exit
 
 from docxtpl import DocxTemplate
-from jinja2 import exceptions as je
+from jinja2 import Environment, exceptions as je
 from json import JSONDecodeError, load
 
-import jinja2
-
+import filters
 
 HELP_MESSAGE = (
     "Jinja2 template processor for DOCX files.\n"
@@ -21,72 +21,54 @@ SYNTAX_ERROR = (
 UNDEFINED_ERROR = "Ошибка определения в шаблоне: {message}"
 TEMPLATE_ERROR = "Ошибка обработки шаблона: {message}"
 GENERAL_ERROR = "Ошибка: {message}"
-               
 
-def s(value):
-    if value is None:
-        return ""
-    else:
-        return int(round(value, 0))
+def main():
+    if len(argv) < 4:
+        print(HELP_MESSAGE)
+        exit(1)
 
+    json_file = argv[1]
+    template = argv[2]
+    out_file = argv[3]
 
-def zs(value):
-    if not value:
-        return ""
-    else:
-        return int(round(value, 0))
+    try:
+        if (template == out_file):
+            raise ValueError(TEMPLATE_NAME_ERROR)
+        
+        environment = Environment()   
+        
+        for name, fun in getmembers(filters, isfunction):
+            environment.filters[name] = fun
+        
+        doc = DocxTemplate(template)
 
+        with open(json_file, "r", encoding="utf8") as file:
+            json_data = load(file)
+        doc.render(context=json_data, jinja_env=environment)
+        doc.save(out_file)
+        return
 
-def d(value):
-    if value is None:
-        return "-"
-    else:
-        return int(round(value, 0))
+    except JSONDecodeError as error:
+        return JSON_ERROR.format(message=error)
+    except je.UndefinedError as error:
+        return UNDEFINED_ERROR.format(message=error.message)
+    except je.TemplateError as error:
+        if hasattr(error, "docx_context") and hasattr(error, "lineno"):
+            lines = [line for line in error.docx_context]
+            index = 3 + min(error.lineno - 4, 0)
+            return SYNTAX_ERROR.format(
+                message=error.message,
+                context=lines[index] if abs(index) < len(lines) else ""
+            )
+        else:
+            return TEMPLATE_ERROR.format(message=error)
+    except Exception as error:
+        return GENERAL_ERROR.format(message=error)
+    
 
-
-def zd(value):
-    if not value:
-        return "-"
-    else:
-        return int(round(value, 0))
-
-
-if len(argv) < 4:
-    print(HELP_MESSAGE)
-    exit(1)
-
-json_file = argv[1]
-template = argv[2]
-out_file = argv[3]
-
-try:
-    if (template == out_file):
-        raise ValueError(TEMPLATE_NAME_ERROR)
-
-    environment = jinja2.Environment()
-    environment.filters['s'] = s
-    environment.filters['zs'] = zs
-    environment.filters['d'] = d
-    environment.filters['zd'] = zd
-    doc = DocxTemplate(template)
-
-    with open(json_file, "r", encoding="utf8") as file:
-        json_data = load(file)
-    doc.render(context=json_data, jinja_env=environment)
-    doc.save(out_file)
+if __name__ == "__main__":
+    message = main()
+    if (message):
+        print(message)
+        exit(1)
     exit(0)
-
-except JSONDecodeError as error:
-    print(JSON_ERROR.format(message=error))
-except je.UndefinedError as error:
-    print(SYNTAX_ERROR.format(message=error.message))
-except je.TemplateError as error:
-    if hasattr(error, "docx_context") and hasattr(error, "lineno"):
-        line = [line for line in error.docx_context][3 + min(error.lineno - 4, 0)]
-        print(SYNTAX_ERROR.format(message=error.message, context=line))
-    else:
-        print(TEMPLATE_ERROR(message=error))
-except Exception as error:
-    print(GENERAL_ERROR.format(message=error))
-
-exit(1)
