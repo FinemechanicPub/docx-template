d70bea2c230fa09c4c57450dfe07df3b8ea27dea
Alexander Rubtsov <93194456+FinemechanicPub@users.noreply.github.com>
2025-02-07 12:18:31 +0300

    Do not convert special floating point values

--- filters.py
+++ filters.py
@@ -1,29 +1,30 @@
 """Дополнительные фильтры для Jinja2"""
 
+NO_CONVERT = [float("inf"), float("-inf"), float("nan")]
 
 def s(value):
     if value is None:
         return ""
     else:
-        return int(round(value, 0))
+        return int(round(value, 0)) if value not in NO_CONVERT else value
 
 
 def zs(value):
     if not value:
         return ""
     else:
-        return int(round(value, 0))
+        return int(round(value, 0)) if value not in NO_CONVERT else value
 
 
 def d(value):
     if value is None:
         return "-"
     else:
-        return int(round(value, 0))
+        return int(round(value, 0)) if value not in NO_CONVERT else value
 
 
 def zd(value):
     if not value:
         return "-"
     else:
-        return int(round(value, 0))
+        return int(round(value, 0)) if value not in NO_CONVERT else value
